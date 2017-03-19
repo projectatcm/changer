b29c039e0bc95c0b4cbd8e2703564dbc54664ced
prasanthnv <prasanthcodemagos@gmail.com>
2017-03-19 08:50:05 +0530

    contats added

---
+++ app/src/main/java/com/codemagos/profilechanger/Adapters/ContactsListAdapter.java
@@ -0,0 +1,8 @@
+package com.codemagos.profilechanger.Adapters;
+
+/**
+ * Created by prasanth on 19/3/17.
+ */
+
+public class ContactsListAdapter {
+}
---
+++ app/src/main/java/com/codemagos/profilechanger/ContactsListActivity.java
@@ -0,0 +1,13 @@
+package com.codemagos.profilechanger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_list);
+    }
+}
